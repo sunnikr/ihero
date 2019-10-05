9733d84822c460dad733b7f56f741cdc5894d617
unknown <sandeepnambath@gmail.com>
2019-10-05 09:56:44 -0400

    oct 5 commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,28 @@
 const express = require('express');
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('profile route'));
+//@route get api/profile/me
+//@desc get current user profile
+//@access Public
+
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile)
+      return res.status(400).json({ msg: 'there is no profile for this user' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500);
+  }
+});
 
 module.exports = router;
